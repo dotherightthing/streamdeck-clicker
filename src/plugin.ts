753d8110d0b6e9d46bfcaa8102e09e0e347c4d1a
Dan Smith <dev@dotherightthing.co.nz>
2024-01-12 22:20:04 +1300

    Remove Text action (fixes #2, fixes #7)

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,7 +4,6 @@ import { C } from "./actions/c";
 import { DC } from "./actions/dc";
 import { M } from "./actions/m";
 import { RC } from "./actions/rc";
-import { T } from "./actions/t";
 import { TC } from "./actions/tc";
 
 // Enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
@@ -15,7 +14,6 @@ streamDeck.actions.registerAction(new C());
 streamDeck.actions.registerAction(new DC());
 streamDeck.actions.registerAction(new M());
 streamDeck.actions.registerAction(new RC());
-streamDeck.actions.registerAction(new T());
 streamDeck.actions.registerAction(new TC());
 
 // Connect to the Stream Deck.
