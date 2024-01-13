d72b7c34cc16ed7494246f58a5f9dff064214b77
Dan Smith <dev@dotherightthing.co.nz>
2024-01-13 21:41:28 +1300

    Automatically use absolute positioning for negative values (fixes #1)

--- src/modules/cliclick.mjs
+++ src/modules/cliclick.mjs
@@ -21,10 +21,14 @@ export async function cliclick(cmd, settings) {
 
   let command = 'libs/cliclick/cliclick';
 
+  // If you need to specify absolute negative values in case you have a setup with a second display arranged to the left of your main display, prefix the number with “=”, for instance “c:100,=-200”.
+  let xStr = (x < 0) ? `=${x}` : x;
+  let yStr = (y < 0) ? `=${y}` : y;
+
   command += restore ? ' -r' : '';
   command += (easing > 0) ? ` -e ${easing}` : '';
   command += ` -w ${wait}`;
-  command += ` ${cmd}:=${x},=${y}`;
+  command += ` ${cmd}:${xStr},${yStr}`;
 
 
   // https://github.com/sindresorhus/execa#handling-errors
