8ff2de7ebeef67493d9f99c727e6dfafe328e93e
Dan Smith <dev@dotherightthing.co.nz>
2024-01-13 16:00:13 +1300

    Lint JS

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,42 +12,42 @@ const sdPlugin = "com.dtrt.clicker.sdPlugin";
  * @type {import('rollup').RollupOptions}
  */
 const config = {
-	input: "src/plugin.ts",
-	output: {
-		file: `${sdPlugin}/bin/plugin.js`,
-		sourcemap: isWatching,
-		sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
-			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href;
-		}
-	},
-	plugins: [
-		{
-			name: "watch-externals",
-			buildStart: function () {
-				this.addWatchFile(`${sdPlugin}/manifest.json`);
-				this.addWatchFile(`${sdPlugin}/property-inspector/inspector.css`);
-				this.addWatchFile(`${sdPlugin}/property-inspector/inspector.html`);
-				this.addWatchFile(`${sdPlugin}/property-inspector/inspector.js`);
-			},
-		},
-		typescript({
-			mapRoot: isWatching ? "./" : undefined
-		}),
-		nodeResolve({
-			browser: false,
-			exportConditions: ["node"],
-			modulePaths: ["./src/modules"],
-			preferBuiltins: true
-		}),
-		commonjs(),
-		!isWatching && terser(),
-		{
-			name: "emit-module-package-file",
-			generateBundle() {
-				this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
-			}
-		}
-	]
+  input: "src/plugin.ts",
+  output: {
+    file: `${sdPlugin}/bin/plugin.js`,
+    sourcemap: isWatching,
+    sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+      return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href;
+    }
+  },
+  plugins: [
+    {
+      name: "watch-externals",
+      buildStart: function () {
+        this.addWatchFile(`${sdPlugin}/manifest.json`);
+        this.addWatchFile(`${sdPlugin}/property-inspector/inspector.css`);
+        this.addWatchFile(`${sdPlugin}/property-inspector/inspector.html`);
+        this.addWatchFile(`${sdPlugin}/property-inspector/inspector.js`);
+      },
+    },
+    typescript({
+      mapRoot: isWatching ? "./" : undefined
+    }),
+    nodeResolve({
+      browser: false,
+      exportConditions: [ "node" ],
+      modulePaths: [ "./src/modules" ],
+      preferBuiltins: true
+    }),
+    commonjs(),
+    !isWatching && terser(),
+    {
+      name: "emit-module-package-file",
+      generateBundle() {
+        this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
+      }
+    }
+  ]
 };
 
 export default config;
